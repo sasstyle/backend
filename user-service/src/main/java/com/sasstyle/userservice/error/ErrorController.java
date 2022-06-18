45fd841c2e6e788fca61de5c7c5f03f3265122a3
kingjaewon2000 <kkkxx_00@naver.com>
2022-06-18 23:21:20 +0900

    feat: :sparkles: ErrorController(UserNotFoundException) 추가

--- user-service/src/main/java/com/sasstyle/userservice/error/ErrorController.java
+++ user-service/src/main/java/com/sasstyle/userservice/error/ErrorController.java
@@ -1,6 +1,7 @@
 package com.sasstyle.userservice.error;
 
 import com.sasstyle.userservice.error.exception.DuplicatedException;
+import com.sasstyle.userservice.error.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,6 +16,11 @@ public class ErrorController {
      * 스프링 유저 서비스 에러 처리 핸들러
      * @return ErrorResponse
      */
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler
+    public ErrorResponse userNotFoundException(UserNotFoundException e) {
+        return new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
