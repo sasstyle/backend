c9c403336e853b0979c8d5e1ceccf0efaf0c1db6
김태형 <taehyeonggim44@gmail.com>
2022-07-16 01:07:38 +0900

    feat: :memo: 상품 좋아요 기능 스웨거 추가

--- product-service/src/main/java/com/sasstyle/productservice/controller/ProductWishController.java
+++ product-service/src/main/java/com/sasstyle/productservice/controller/ProductWishController.java
@@ -1,9 +1,10 @@
 package com.sasstyle.productservice.controller;
 
 import com.sasstyle.productservice.controller.dto.ProductResponse;
-import com.sasstyle.productservice.controller.dto.WishIdResponse;
 import com.sasstyle.productservice.controller.dto.WishRequest;
 import com.sasstyle.productservice.service.ProductWishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,18 +19,27 @@ public class ProductWishController {
 
     private final ProductWishService productWishService;
 
+    @Operation(summary = "위시리스트 상품 목록", description = "사용자가 좋아요를 누른 상품 목록을 출력합니다.")
+    @ApiResponse(responseCode = "200", description = "위시리스트 상품 목록 반환 성공")
     @GetMapping("/me")
     public ResponseEntity<Page<ProductResponse>> wishList(@RequestHeader String userId, Pageable pageable) {
         return ResponseEntity
                 .ok(productWishService.findWishList(userId, pageable));
     }
 
+    @Operation(summary = "상품 좋아요", description = "사용자가 원하는 상품을 좋아요를 누를 수 있습니다.")
+    @ApiResponse(responseCode = "200", description = "상품 좋아요 성공")
     @PostMapping
-    public ResponseEntity<WishIdResponse> wish(@RequestHeader String userId, @RequestBody WishRequest request) {
+    public ResponseEntity<Void> wish(@RequestHeader String userId, @RequestBody WishRequest request) {
+        productWishService.wish(userId, request.getProductId());
+
         return ResponseEntity
-                .ok(new WishIdResponse(productWishService.wish(userId, request.getProductId())));
+                .ok()
+                .build();
     }
 
+    @Operation(summary = "상품 좋아요 삭제", description = "사용자가 상품을 좋아요를 삭제할 수 있습니다.")
+    @ApiResponse(responseCode = "200", description = "상품 좋아요 삭제 성공")
     @DeleteMapping
     public ResponseEntity<Void> unWish(@RequestHeader String userId, @RequestBody WishRequest request) {
         productWishService.unWish(userId, request.getProductId());
--- product-service/src/main/java/com/sasstyle/productservice/controller/dto/WishRequest.java
+++ product-service/src/main/java/com/sasstyle/productservice/controller/dto/WishRequest.java
@@ -1,5 +1,6 @@
 package com.sasstyle.productservice.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,5 +10,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class WishRequest {
 
+    @Schema(description = "상품 아이디", example = "1", required = true)
     private Long productId;
 }
