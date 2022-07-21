b9c4a73f8cd6a2003910f77ad73cd1311a116bd8
김태형 <taehyeonggim44@gmail.com>
2022-07-22 02:48:57 +0900

    fix: :bug: 장바구니 조회시 상품 아이디, 브랜드 이름 조회 추가

--- cart-service/src/main/java/com/sasstyle/cartservice/controller/dto/CartDetailResponse.java
+++ cart-service/src/main/java/com/sasstyle/cartservice/controller/dto/CartDetailResponse.java
@@ -11,11 +11,17 @@ public class CartDetailResponse {
     @Schema(description = "장바구니 상품 상세 아이디", example = "2", required = true)
     private Long cartDetailId;
 
+    @Schema(description = "상품 아이디", example = "3", required = true)
+    private Long productId;
+
     @Schema(description = "상품 프로필", example = "https://picsum.photos/seed/picsum/200/300", required = true)
     private String profileUrl;
 
     @Schema(description = "상품 이름", example = "레이어드 슬리브리스 탑_일반 기장 [화이트]", required = true)
     private String name;
+
+    @Schema(description = "브랜드 이름", example = "싸스타일", required = true)
+    private String brandName;
 
     @Schema(description = "상품 가격", example = "11900", required = true)
     private int price;
