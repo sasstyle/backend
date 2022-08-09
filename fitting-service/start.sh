5fbcf0f76a3a0daecc1a03b0a9209732b2a068a3
myunbongs <myunbongs@naver.com>
2022-08-10 00:54:08 +0900

    del: 🗑  unet 파일 다운로드 커맨드 삭제

--- fitting-service/start.sh
+++ fitting-service/start.sh
@@ -1,6 +1,3 @@
-gdown --id 1rbSTGKAE-MTxBYHd-51l2hMOQPT_7EPy -O ACGPN/U_2_Net/saved_models/u2netp/u2netp.pth
-gdown --id 1ao1ovG1Qtx4b7EoskHXmi2E9rp5CHLcZ -O ACGPN/U_2_Net/saved_models/saved_models/u2net/u2net.pth
-
 python manage.py migrate
 python manage.py runserver 0.0.0.0:9002
 
